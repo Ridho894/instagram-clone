d27fda82bdc89eba76c725c6de110ca2316bd820
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2021-10-20 21:25:55 +0700

    header 100%

--- components/Header.js
+++ components/Header.js
@@ -7,15 +7,14 @@ import {
   PaperAirplaneIcon,
   MenuIcon,
 } from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
 
 function Header() {
   return (
-    <div>
-      <div className={"flex justify-between max-w-6xl"}>
+    <div className={"shadow-sm border-b bg-white sticky top-0 z-50"}>
+      <div className={"flex justify-between max-w-6xl mx-5 lg:mx-auto"}>
         {/* Left */}
-        <div
-          className={"relative hidden lg:inline-grid w-24 cursor-pointer"}
-        >
+        <div className={"relative hidden lg:inline-grid w-24 cursor-pointer"}>
           <Image
             src="https://links.papareact.com/ocw"
             layout="fill"
@@ -23,11 +22,7 @@ function Header() {
           />
         </div>
 
-        <div
-          className={
-            "relative w-10 lg:hidden flex-shrink-0 cursor-pointer"
-          }
-        >
+        <div className={"relative w-10 lg:hidden flex-shrink-0 cursor-pointer"}>
           <Image
             src="https://links.papareact.com/jjm"
             layout="fill"
@@ -35,17 +30,48 @@ function Header() {
           />
         </div>
         {/* Middle - Search Input Field */}
-        <div className={"relative mt-1 p-3 rounded-md"}>
-          <div
-            className={
-              "absolute inset-y-0 pl-3 flex items-center pointer-events-none"
-            }
-          >
-            <SearchIcon className={"h-5 w-5 text-gray-500"} />
+        <div className={"max-w-xs"}>
+          <div className={"relative mt-1 p-3 rounded-md"}>
+            <div
+              className={
+                "absolute inset-y-0 pl-3 flex items-center pointer-events-none"
+              }
+            >
+              <SearchIcon className={"h-5 w-5 text-gray-500"} />
+            </div>
+            <input
+              className={
+                "bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:border-black focus:ring-black rounded-md"
+              }
+              placeholder="Search"
+              type="text"
+            />
           </div>
-          <input className={"bg-gray-50 block w-full pl-10 sm:text-sm border-gray focus:border-black focus:ring-black rounded-md"} placeholder="Search" type="text" />
         </div>
         {/* Right */}
+        <div className={"flex items-center justify-end space-x-4"}>
+          <HomeIcon className={"navBtn"} />
+          <MenuIcon className={"h-16 md:hidden cursor-pointer"} />
+          <div className={"relative navBtn"}>
+            <PaperAirplaneIcon className={"navBtn rotate-45"} />
+            <div
+              className={
+                "absolute -top-1 -right-2 text-xs text-white w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse"
+              }
+            >
+              3
+            </div>
+          </div>
+          <PlusCircleIcon className={"navBtn"} />
+          <UserGroupIcon className={"navBtn"} />
+          <HeartIcon className={"navBtn"} />
+
+          <img
+            src="https://img.freepik.com/free-photo/surprised-happy-bearded-man-shirt-pointing-away_171337-5021.jpg?size=338&ext=jpg"
+            alt="profile_pic"
+            className={"h-10 rounded-full cursor-pointer object-cover"}
+          />
+        </div>
       </div>
     </div>
   );
