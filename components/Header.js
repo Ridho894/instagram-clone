973329523ccbc9d8c2a3e9dd8bd6f298d8199bac
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2021-10-30 08:23:00 +0700

    login logout with firebase complete

--- components/Header.js
+++ components/Header.js
@@ -8,8 +8,11 @@ import {
   MenuIcon,
 } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 function Header() {
+  const { data: session } = useSession();
+
   return (
     <div className={"shadow-sm border-b bg-white sticky top-0 z-50"}>
       <div className={"flex justify-between max-w-6xl mx-5 lg:mx-auto"}>
@@ -52,25 +55,32 @@ function Header() {
         <div className={"flex items-center justify-end space-x-4"}>
           <HomeIcon className={"navBtn"} />
           <MenuIcon className={"h-16 md:hidden cursor-pointer"} />
-          <div className={"relative navBtn"}>
-            <PaperAirplaneIcon className={"navBtn rotate-45"} />
-            <div
-              className={
-                "absolute -top-1 -right-2 text-xs text-white w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse"
-              }
-            >
-              3
-            </div>
-          </div>
-          <PlusCircleIcon className={"navBtn"} />
-          <UserGroupIcon className={"navBtn"} />
-          <HeartIcon className={"navBtn"} />
+          {session ? (
+            <>
+              <div className={"relative navBtn"}>
+                <PaperAirplaneIcon className={"navBtn rotate-45"} />
+                <div
+                  className={
+                    "absolute -top-1 -right-2 text-xs text-white w-5 h-5 bg-red-500 rounded-full flex items-center justify-center animate-pulse"
+                  }
+                >
+                  3
+                </div>
+              </div>
+              <PlusCircleIcon className={"navBtn"} />
+              <UserGroupIcon className={"navBtn"} />
+              <HeartIcon className={"navBtn"} />
 
-          <img
-            src="https://img.freepik.com/free-photo/surprised-happy-bearded-man-shirt-pointing-away_171337-5021.jpg?size=338&ext=jpg"
-            alt="profile_pic"
-            className={"h-10 rounded-full cursor-pointer object-cover"}
-          />
+              <img
+                onClick={signOut}
+                src={session.user.image}
+                alt="profile_pic"
+                className={"h-10 rounded-full cursor-pointer object-cover"}
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign In</button>
+          )}
         </div>
       </div>
     </div>
---
+++ firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getFirestore } from "firebase/fisrestore";
+import { getStorage } from "firebase/storage";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB6F3lakSRBlD3dadrPSgt6hZnjefUaESs",
+  authDomain: "instagram-v1-build.firebaseapp.com",
+  projectId: "instagram-v1-build",
+  storageBucket: "instagram-v1-build.appspot.com",
+  messagingSenderId: "1011741213379",
+  appId: "1:1011741213379:web:10d0a9d5cb41b9ee9c1876",
+  measurementId: "G-S8YTE2S3T9",
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+
+export { app, db, storage };
